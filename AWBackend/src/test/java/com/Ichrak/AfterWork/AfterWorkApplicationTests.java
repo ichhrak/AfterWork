35bd83a37ddc1420413aa34aee870454475956be
ichhrak <hatimiichrak@gmail.com>
2024-06-30 11:39:44 -0400

    KT-23

---
+++ AWBackend/src/test/java/com/Ichrak/AfterWork/AfterWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ichrak.AfterWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
